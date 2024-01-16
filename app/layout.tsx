049adcc9d455007dd452055ac348d27819737e4d
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-01-16 18:00:07 +0300

    Fix import statement in layout.tsx to correctly reference global.scss file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '..scss/global.scss'
+import '../scss/global.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
